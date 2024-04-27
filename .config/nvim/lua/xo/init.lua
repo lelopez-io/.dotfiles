be085d166514f876dd68a7624385e85a9a1de45e
Luis E. Lopez <ux@lelopez.io>
2024-04-26 23:37:05 -0600

    Update

--- .config/nvim/lua/xo/init.lua
+++ .config/nvim/lua/xo/init.lua
@@ -1,13 +1,31 @@
-require("xo.lazy_init")
-require("xo.netrw")
-require("xo.remap")
 require("xo.set")
+require("xo.remap")
 
 local augroup = vim.api.nvim_create_augroup
-local xoGroup = augroup('xo', {})
+local xogroup = augroup('xo', {})
+local hygroup = augroup('HighlightYank', {})
 
 local autocmd = vim.api.nvim_create_autocmd
-local yank_group = augroup('HighlightYank', {})
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    spec = "xo.lazy",
+    change_detection = { notify = false }
+})
+
+
 
 function R(name)
     require("plenary.reload").reload_module(name)
@@ -20,7 +38,7 @@ vim.filetype.add({
 })
 
 autocmd('TextYankPost', {
-    group = yank_group,
+    group = hygroup,
     pattern = '*',
     callback = function()
         vim.highlight.on_yank({
@@ -31,13 +49,13 @@ autocmd('TextYankPost', {
 })
 
 autocmd({"BufWritePre"}, {
-    group = xoGroup,
+    group = xogroup,
     pattern = "*",
     command = [[%s/\s\+$//e]],
 })
 
 autocmd('LspAttach', {
-    group = xoGroup,
+    group = xogroup,
     callback = function(e)
         local opts = { buffer = e.buf }
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
@@ -52,3 +70,7 @@ autocmd('LspAttach', {
         vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
     end
 })
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
