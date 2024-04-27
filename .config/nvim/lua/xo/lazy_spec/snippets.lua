5adea47840cff7ea2adede063dbf2ee69ee3dfcd
Luis E. Lopez <ux@lelopez.io>
2024-04-27 01:26:30 -0600

    TODO

--- .config/nvim/lua/xo/lazy_spec/snippets.lua
+++ .config/nvim/lua/xo/lazy_spec/snippets.lua
@@ -4,8 +4,9 @@ return {
         "L3MON4D3/LuaSnip",
         -- follow latest release.
         version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- TODO: debug jsregexp install
         -- install jsregexp (optional!).
-        build = "make install_jsregexp",
+        -- build = "make install_jsregexp", 
 
         dependencies = { "rafamadriz/friendly-snippets" },
 
