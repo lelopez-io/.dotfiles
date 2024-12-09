62a325d476ebf489171af82219740745cb75143b
Luis E. Lopez <ux@lelopez.io>
2024-12-09 16:38:39 -0700

    chore: consistent formatting

--- .config/nvim/lua/xo/config/keymaps.lua
+++ .config/nvim/lua/xo/config/keymaps.lua
@@ -2,7 +2,11 @@
 vim.g.mapleader = " "
 
 -- File explorer
-vim.keymap.set("n", "<leader>jj", vim.cmd.Ex, { desc = "Open file explorer (netrw)" })
+vim.keymap.set("n", "<leader>jj", vim.cmd.Ex, {
+    noremap = false,
+    silent = false,
+    desc = "Open file explorer (netrw)",
+})
 
 -- Indentation controls
 vim.keymap.set("i", "<S-Tab>", "<C-d>", {
@@ -29,56 +33,132 @@ vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>", {
 })
 
 -- Move selected lines up/down in visual mode
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected lines down" })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected lines up" })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", {
+    noremap = false,
+    silent = false,
+    desc = "Move selected lines down",
+})
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", {
+    noremap = false,
+    silent = false,
+    desc = "Move selected lines up",
+})
 
 -- Better line joins and scrolling
-vim.keymap.set("n", "J", "mzJ`z", { desc = "Join lines and keep cursor position" })
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center cursor" })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center cursor" })
-vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result and center cursor" })
-vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result and center cursor" })
+vim.keymap.set("n", "J", "mzJ`z", {
+    noremap = false,
+    silent = false,
+    desc = "Join lines and keep cursor position",
+})
+vim.keymap.set("n", "<C-d>", "<C-d>zz", {
+    noremap = false,
+    silent = false,
+    desc = "Scroll down and center cursor",
+})
+vim.keymap.set("n", "<C-u>", "<C-u>zz", {
+    noremap = false,
+    silent = false,
+    desc = "Scroll up and center cursor",
+})
+vim.keymap.set("n", "n", "nzzzv", {
+    noremap = false,
+    silent = false,
+    desc = "Next search result and center cursor",
+})
+vim.keymap.set("n", "N", "Nzzzv", {
+    noremap = false,
+    silent = false,
+    desc = "Previous search result and center cursor",
+})
 
 -- Paste over selection without copying it
-vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste without yanking selection" })
+vim.keymap.set("x", "<leader>p", [["_dP]], {
+    noremap = false,
+    silent = false,
+    desc = "Paste without yanking selection",
+})
 
 -- System clipboard operations
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
-vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank line to system clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], {
+    noremap = false,
+    silent = false,
+    desc = "Yank to system clipboard",
+})
+vim.keymap.set("n", "<leader>Y", [["+Y]], {
+    noremap = false,
+    silent = false,
+    desc = "Yank line to system clipboard",
+})
 
 -- Delete without yanking
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without yanking" })
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], {
+    noremap = false,
+    silent = false,
+    desc = "Delete without yanking",
+})
 
 -- Disable Ex mode
-vim.keymap.set("n", "Q", "<nop>", { desc = "Disable Ex mode" })
+vim.keymap.set("n", "Q", "<nop>", {
+    noremap = false,
+    silent = false,
+    desc = "Disable Ex mode",
+})
 
 -- Quickfix navigation
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz", { desc = "Next quickfix item" })
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz", { desc = "Previous quickfix item" })
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz", { desc = "Next location list item" })
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz", { desc = "Previous location list item" })
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz", {
+    noremap = false,
+    silent = false,
+    desc = "Next quickfix item",
+})
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz", {
+    noremap = false,
+    silent = false,
+    desc = "Previous quickfix item",
+})
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz", {
+    noremap = false,
+    silent = false,
+    desc = "Next location list item",
+})
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz", {
+    noremap = false,
+    silent = false,
+    desc = "Previous location list item",
+})
 
 -- Quick actions
-vim.keymap.set(
-    "n",
-    "<leader>s",
-    [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
-    { desc = "Search and replace word under cursor" }
-)
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], {
+    noremap = false,
+    silent = false,
+    desc = "Search and replace word under cursor",
+})
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", {
+    noremap = false,
     silent = true,
     desc = "Make current file executable",
 })
 
 -- Go error handling snippet
-vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>", { desc = "Insert Go error handling" })
+vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>", {
+    noremap = false,
+    silent = false,
+    desc = "Insert Go error handling",
+})
 
 -- Format current buffer
 vim.keymap.set("n", "<leader>f", function()
     vim.lsp.buf.format()
-end, { desc = "Format current buffer" })
+end, {
+    noremap = false,
+    silent = false,
+    desc = "Format current buffer",
+})
 
 -- Source current file
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
-end, { desc = "Source current file" })
+end, {
+    noremap = false,
+    silent = false,
+    desc = "Source current file",
+})
