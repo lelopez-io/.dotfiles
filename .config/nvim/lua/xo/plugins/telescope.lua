96dc4286a387ed788366f9d6f8d6df85f05cc1a2
Luis E. Lopez <ux@lelopez.io>
2024-12-11 13:59:03 -0700

    feat: update key map format

--- .config/nvim/lua/xo/plugins/telescope.lua
+++ .config/nvim/lua/xo/plugins/telescope.lua
@@ -33,22 +33,53 @@ return {
         telescope.load_extension("fzf")
 
         local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>kk", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>ki", builtin.git_files, {})
+
+        -- Telescope keymaps
+        vim.keymap.set("n", "<leader>kk", builtin.find_files, {
+            noremap = false,
+            silent = false,
+            desc = "Find files in current directory (includes hidden files)",
+        })
+
+        vim.keymap.set("n", "<leader>ki", builtin.git_files, {
+            noremap = false,
+            silent = false,
+            desc = "Find files tracked by git",
+        })
+
         vim.keymap.set("n", "<leader>kws", function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
-        end)
+        end, {
+            noremap = false,
+            silent = false,
+            desc = "Search for word under cursor",
+        })
+
         vim.keymap.set("n", "<leader>kWs", function()
             local word = vim.fn.expand("<cWORD>")
             builtin.grep_string({ search = word })
-        end)
+        end, {
+            noremap = false,
+            silent = false,
+            desc = "Search for WORD under cursor (includes more characters)",
+        })
+
         vim.keymap.set("n", "<leader>ksa", function()
             builtin.grep_string({
                 search = vim.fn.input("Grep > "),
                 additional_args = { "--hidden" },
             })
-        end)
-        vim.keymap.set("n", "<leader>kh", builtin.help_tags, {})
+        end, {
+            noremap = false,
+            silent = false,
+            desc = "Interactive search (grep) through all files",
+        })
+
+        vim.keymap.set("n", "<leader>kh", builtin.help_tags, {
+            noremap = false,
+            silent = false,
+            desc = "Search through Neovim help tags",
+        })
     end,
 }
