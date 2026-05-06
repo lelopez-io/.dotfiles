58f554005622cd0135195d987bdbc961575e793d
Luis E. Lopez <ux@lelopez.io>
2026-05-06 10:49:35 -0600

    fix(nvim): pin nvim-treesitter to main branch for neovim 0.12+ compatibility
    nvim-treesitter dropped the master branch in favor of main for neovim 0.12+ support. also adds hererocks and plenary.nvim to the lockfile and updates all plugin commits, and migrates the custom templ parser registration to use the new api via a TSUpdate autocmd.

--- .config/nvim/lua/xo/plugins/treesitter.lua
+++ .config/nvim/lua/xo/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",  -- Required for Neovim 0.12+
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
@@ -38,14 +39,19 @@ return {
             },
         })
 
-        local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-        treesitter_parser_config.templ = {
-            install_info = {
-                url = "https://github.com/vrischmann/tree-sitter-templ.git",
-                files = { "src/parser.c", "src/scanner.c" },
-                branch = "master",
-            },
-        }
+        -- Register custom templ parser (new API for nvim-treesitter main branch)
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "TSUpdate",
+            callback = function()
+                require("nvim-treesitter.parsers").templ = {
+                    install_info = {
+                        url = "https://github.com/vrischmann/tree-sitter-templ",
+                        revision = "68d6707ed20159ae7525241b8161c126dc1c620c",
+                        files = { "src/parser.c", "src/scanner.c" },
+                    },
+                }
+            end,
+        })
 
         vim.treesitter.language.register("templ", "templ")
     end,
